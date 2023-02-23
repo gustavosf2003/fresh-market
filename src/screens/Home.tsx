164f1996eb958560a059d47a4f736ab11958eac1
gustavosf2003 <gsferreira2003@gmail.com>
2023-02-23 00:18:04 +0000

    feat:show product cards and save values in storage

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,17 +1,60 @@
-import React, { useState } from "react";
-import { SafeAreaView, View, Text, ScrollView } from "react-native";
+import React, { useEffect, useState } from "react";
+import { SafeAreaView, View, Text, ScrollView, Button } from "react-native";
 import Layout from "../components/Layout";
 import ScrollList from "../components/ScrollList";
+import ProductCard from "../components/ProductCard";
+import {
+  removeStorageData,
+  getStorageData,
+  setStorageData,
+  StorageKeys,
+} from "../../utils/storage";
+const categories = [
+  "vegetables",
+  "legumes",
+  "snacks",
+  "meat",
+  "bakery",
+  "fruits",
+];
+const productsList = [
+  {
+    id: 0,
+    name: "Tomato",
+    origin: "Spain",
+    image: "Spain",
+    price: 30.0,
+  },
+  {
+    id: 1,
+    image: "Potato",
+    name: "Potato",
+    origin: "Portugal",
+    price: 20.0,
+  },
+];
+async function getStorageCart(
+  setCart: React.Dispatch<React.SetStateAction<never[]>>
+) {
+  const cartData = await getStorageData(StorageKeys.products);
+  if (cartData && JSON.parse(cartData).length > 0) {
+    setCart(JSON.parse(cartData));
+  } else {
+    setCart([]);
+  }
+}
 const Home: React.FC = () => {
   const [category, setCategory] = useState("vegetables");
-  const categories = [
-    "vegetables",
-    "legumes",
-    "snacks",
-    "meat",
-    "bakery",
-    "fruits",
-  ];
+  const [cart, setCart] = useState([]);
+  console.log(cart);
+  useEffect(() => {
+    getStorageCart(setCart);
+  }, []);
+  useEffect(() => {
+    if (cart.length > 0) {
+      setStorageData(StorageKeys.products, JSON.stringify(cart));
+    }
+  }, [cart]);
   return (
     <>
       <Layout>
@@ -21,10 +64,22 @@ const Home: React.FC = () => {
           categoryChange={setCategory}
         />
         <View className={`border-b border-gray-300 my-3`} />
-        <View className="mx-5">
-          <Text className="text-xl font-bold">290+ Results</Text>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ paddingBottom: 320 }}
+          className="mx-5"
+        >
+          <Text className="text-xl font-bold">+256 products</Text>
           <Text className="text-xl font-bold">{category}</Text>
-        </View>
+          {productsList.map((product) => (
+            <ProductCard
+              key={product.id}
+              cart={cart}
+              setCart={setCart}
+              product={product}
+            />
+          ))}
+        </ScrollView>
       </Layout>
     </>
   );
