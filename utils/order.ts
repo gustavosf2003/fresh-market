f5e4554fb25471156304755b81731b6fcd02f309
gustavosf2003 <gsferreira2003@gmail.com>
2023-02-28 23:30:21 +0000

    fix: add price to order json

--- utils/order.ts
+++ utils/order.ts
@@ -1,23 +1,27 @@
 import { Product } from "../interfaces/products";
+import { calculateTotalCost } from "./prices";
 
 interface Order {
   products: Product[];
   tips: number;
   deliveryFee: number;
   discount: number;
+  price: number;
 }
 
 export function sendOrder(
   products: Product[],
   tips: number,
   deliveryFee: number,
-  discount: number
+  discount: number,
+  price: number
 ) {
   let order: Order = {
     products: products,
     tips: tips,
     deliveryFee: deliveryFee,
     discount: discount,
+    price: calculateTotalCost(products, deliveryFee, discount),
   };
   console.log(order);
 }
