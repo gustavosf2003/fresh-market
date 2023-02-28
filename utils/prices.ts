d4d75a96cb1fc258a9e52edb62cab3ae461388d5
gustavosf2003 <gsferreira2003@gmail.com>
2023-02-28 21:32:05 +0000

    fix: total costs

---
+++ utils/prices.ts
@@ -0,0 +1,29 @@
+import { businessRules } from "@app/config/constants";
+import { Product } from "@app/interfaces/products";
+
+export function calculateSubtotalCost(
+  items: Product[],
+  discount?: number
+): number {
+  let total = 0;
+  for (const item of items) {
+    total += item.price * (item.quantity || 0);
+  }
+  if (discount) {
+    return total - discount + businessRules.deliveryFee;
+  }
+  return total + businessRules.deliveryFee;
+}
+export function calculateTotalCost(
+  items: Product[],
+  discount?: number
+): number {
+  let total = 0;
+  for (const item of items) {
+    total += item.price * (item.quantity || 0);
+  }
+  if (discount) {
+    return total - discount + businessRules.deliveryFee;
+  }
+  return total + businessRules.deliveryFee;
+}
