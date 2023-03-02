caef431ab422035518c0bcd80915dbf63b80f65a
gustavosf2003 <gsferreira2003@gmail.com>
2023-03-02 01:24:00 +0000

    feat: init geocoder

--- App.tsx
+++ App.tsx
@@ -1,7 +1,12 @@
 /// <reference types="nativewind/types" />
 import { NavigationContainer } from "@react-navigation/native";
 import Routes from "./config/routes";
+import { GOOGLE_MAPS_TOKEN } from "@env";
+import Geocoder from "react-native-geocoding";
+import React from "react";
+
 export default function App() {
+  Geocoder.init(GOOGLE_MAPS_TOKEN);
   return (
     <NavigationContainer>
       <Routes />
