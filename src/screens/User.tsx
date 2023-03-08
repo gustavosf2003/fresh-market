ec4a743aea5dfac7fe5d1150a30ff3b9cbca9eb5
gustavosf2003 <gsferreira2003@gmail.com>
2023-03-08 02:59:23 +0000

    fix: edit user page

--- src/screens/User.tsx
+++ src/screens/User.tsx
@@ -1,11 +1,32 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useState } from "react";
+import { Text, View, TouchableOpacity, Image } from "react-native";
 import Layout from "../components/Layout";
+import ProfileCategory from "../components/ProfileCategory";
+import Profile from "../components/Profile";
+import Orders from "../components/Orders";
+
+function getPageContent(category: number) {
+  switch (category) {
+    case 0:
+      return <Profile />;
+    case 1:
+      return <Orders />;
+
+    default:
+      return <Profile />;
+  }
+}
 
 const User: React.FC = () => {
+  const [profileCategory, setProfileCategory] = useState(0);
   return (
     <Layout>
-      <Text>User</Text>
+      <ProfileCategory
+        currentCategory={profileCategory}
+        categoryChange={setProfileCategory}
+      />
+      <View className={`border-b border-gray-300`} />
+      <View className="mx-8 mt-4">{getPageContent(profileCategory)}</View>
     </Layout>
   );
 };
