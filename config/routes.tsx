e9f7e086ce01e7141b1401f8facf3cb4932d0dd6
gustavosf2003 <gsferreira2003@gmail.com>
2023-03-09 01:57:18 +0000

    feat: redirect to home if user is registered

--- config/routes.tsx
+++ config/routes.tsx
@@ -4,21 +4,41 @@ import Home from "../src/screens/Home";
 import Products from "../src/screens/Products";
 import User from "../src/screens/User";
 import { RoutesName } from "@app/config/constants";
+import Register from "@app/src/screens/Register";
+import { getStorageData, StorageKeys } from "../utils/storage";
+import { useState, useEffect } from "react";
 
+async function checkRegistration(): Promise<boolean> {
+  const isRegistered = await getStorageData(StorageKeys.isRegistered);
+  return Boolean(isRegistered);
+}
 const Stack = createNativeStackNavigator();
-const Routes: React.FC = () => {
+const Routes = () => {
+  const [initialRouteName, setInitialRouteName] = useState("");
+
+  useEffect(() => {
+    async function determineInitialRouteName() {
+      const isRegistered = await checkRegistration();
+      setInitialRouteName(isRegistered ? RoutesName.home : RoutesName.register);
+    }
+
+    determineInitialRouteName();
+  }, []);
   return (
-    <Stack.Navigator
-      initialRouteName={RoutesName.home}
-      screenOptions={{
-        headerShown: false,
-        animation: "none",
-      }}
-    >
-      <Stack.Screen name={RoutesName.home} component={Home} />
-      <Stack.Screen name={RoutesName.products} component={Products} />
-      <Stack.Screen name={RoutesName.user} component={User} />
-    </Stack.Navigator>
+    initialRouteName.length > 0 && (
+      <Stack.Navigator
+        initialRouteName={initialRouteName}
+        screenOptions={{
+          headerShown: false,
+          animation: "none",
+        }}
+      >
+        <Stack.Screen name={RoutesName.home} component={Home} />
+        <Stack.Screen name={RoutesName.register} component={Register} />
+        <Stack.Screen name={RoutesName.products} component={Products} />
+        <Stack.Screen name={RoutesName.user} component={User} />
+      </Stack.Navigator>
+    )
   );
 };
 
