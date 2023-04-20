58bc3cd6ccedae079261bfa52cce350cf3eb00e6
gustavosf2003 <gsferreira2003@gmail.com>
2023-04-20 23:19:57 +0100

    fix: hide shopping button

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -25,15 +25,15 @@ const AppBar: React.FC = () => {
         >
           Fresh Market
         </Text>
-        {route.name != RoutesName.products ||
-          (route.name != RoutesName.processing && (
+        {route.name != RoutesName.products &&
+          route.name != RoutesName.processing && (
             <FontAwesome
               onPress={() => navigation.navigate(RoutesName.products as never)}
               name="shopping-basket"
               size={24}
               color="white"
             />
-          ))}
+          )}
       </View>
     </SafeAreaView>
   );
