4ef0ea7e43655d5f2cd02aa456649807bac15b2f
gustavosf2003 <gsferreira2003@gmail.com>
2023-04-22 01:26:40 +0100

    fix: move product context to app.tsx

--- App.tsx
+++ App.tsx
@@ -3,17 +3,22 @@ import { NavigationContainer } from "@react-navigation/native";
 import Routes from "./config/routes";
 import { GOOGLE_MAPS_TOKEN } from "@env";
 import Geocoder from "react-native-geocoding";
-import React from "react";
+import React, { useState } from "react";
 import { ApolloProvider } from "@apollo/react-hooks";
 import { storyBlokClient } from "./services/api";
 import { ThemeProvider } from "react-native-elements";
+import { ProductContext } from "./context/product";
 export default function App() {
   Geocoder.init(GOOGLE_MAPS_TOKEN);
+
+  const [savedProducts, setSavedProducts] = useState([]);
   return (
     <ApolloProvider client={storyBlokClient}>
       <ThemeProvider>
         <NavigationContainer>
-          <Routes />
+          <ProductContext.Provider value={{ savedProducts, setSavedProducts }}>
+            <Routes />
+          </ProductContext.Provider>
         </NavigationContainer>
       </ThemeProvider>
     </ApolloProvider>
