e7a695b572d4a447a874c26a3b5a88da9a3e4d9b
gustavosf2003 <gsferreira2003@gmail.com>
2023-04-24 00:12:45 +0100

    feat: add product modal to searchbar

--- src/components/search/SearchList.tsx
+++ src/components/search/SearchList.tsx
@@ -1,8 +1,18 @@
 import { GET_PRODUCTS } from "@app/queries/products";
-import React from "react";
-import { View, Text, Image, ActivityIndicator } from "react-native";
+import React, { useContext, useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  Image,
+  ActivityIndicator,
+  TouchableOpacity,
+} from "react-native";
 import { useQuery } from "@apollo/react-hooks";
 import { Product } from "../../../interfaces/products";
+import { getQuantityByProductId } from "../ProductCard";
+import { ProductContext } from "@app/context/product";
+import ProductModal from "../ProductModal";
+import Modal from "react-native-modal";
 interface SearchListProps {
   searchText: string;
 }
@@ -11,33 +21,59 @@ const SearchList = ({ searchText }: SearchListProps) => {
   const { loading, error, data } = useQuery(GET_PRODUCTS, {
     variables: { searchTerm: searchText, perPage: 4 },
   });
+  const { savedProducts } = useContext(ProductContext);
+  const [counterValue, setCounterValue] = useState<number>(0);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
+
+  async function openModal(item: Product) {
+    setCounterValue(getQuantityByProductId(savedProducts, item.id)!);
+    setSelectedProduct(item);
+    setIsModalVisible(true);
+  }
+
   let searchedItems;
   if (data) {
     searchedItems = data.ProductItems.items;
   }
   return (
-    <View className="absolute w-full -bottom-[228px] min-h-[228px]">
-      <View className="bg-slate-200 py-2.5 px-2">
-        {loading && <ActivityIndicator />}
-        {error && <Text>There was an error</Text>}
-        {searchedItems?.length == 0 && <Text>No result</Text>}
-        {data &&
-          searchedItems.map((item: Product) => (
-            <View key={item.id} className="flex flex-row items-start mt-3">
-              <Image
-                className="w-10 h-10"
-                source={{ uri: item.content.image.filename }}
-              />
-              <View className="ml-2">
-                <Text className="font-medium capitalize">
-                  {item.content.name}
-                </Text>
-                <Text className="capitalize">{item.content.unit}</Text>
-              </View>
-            </View>
-          ))}
+    <>
+      {selectedProduct && isModalVisible && (
+        <ProductModal
+          product={selectedProduct}
+          isOpen={true}
+          setIsOpen={setIsModalVisible}
+          counterValue={counterValue}
+          setCounterValue={setCounterValue}
+        />
+      )}
+      <View className="absolute w-full -bottom-[228px] min-h-[228px]">
+        <View className="bg-slate-200 py-2.5 px-2">
+          {loading && <ActivityIndicator />}
+          {error && <Text>There was an error</Text>}
+          {searchedItems?.length == 0 && <Text>No result</Text>}
+          {data &&
+            searchedItems.map((item: Product) => (
+              <TouchableOpacity
+                key={item.id}
+                className="flex flex-row items-start mt-3"
+                onPress={() => openModal(item)}
+              >
+                <Image
+                  className="w-10 h-10"
+                  source={{ uri: item.content.image.filename }}
+                />
+                <View className="ml-2">
+                  <Text className="font-medium capitalize">
+                    {item.content.name}
+                  </Text>
+                  <Text className="capitalize">{item.content.unit}</Text>
+                </View>
+              </TouchableOpacity>
+            ))}
+        </View>
       </View>
-    </View>
+    </>
   );
 };
 
