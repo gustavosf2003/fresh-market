ac5861ba6af0d50548dcdc0f2a2c63f11dbd04f9
gustavosf2003 <gsferreira2003@gmail.com>
2023-04-28 01:02:47 +0100

    feat: handlebars

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,17 @@
+const { engine } = require("express-handlebars");
 const express = require("express");
+const { sendOrderConfirmationEmail } = require("./email");
+
 const app = express();
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./views");
 
 app.use(express.json());
 
 app.post("/order", (req, res) => {
-  console.log(req.body);
+  let order = req.body;
+  sendOrderConfirmationEmail(order);
   res.send(`Order done`);
 });
 
