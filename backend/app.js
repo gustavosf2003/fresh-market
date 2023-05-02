8df3603d40938b8c14bbe3a63ab851710af18e2a
gustavosf2003 <gsferreira2003@gmail.com>
2023-05-02 01:06:31 +0100

    feat: create auth routes

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,32 @@
 const { engine } = require("express-handlebars");
 const express = require("express");
+const cors = require("cors");
 const { sendOrderConfirmationEmail } = require("./email");
+const { login, isAuthenticated } = require("./utils/login");
 
 const app = express();
 app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
+app.use(cors());
 app.set("views", "./views");
 const PORT = process.env.PORT || 3030;
 
 app.use(express.json());
+
+app.post("/admin/login", async (req, res) => {
+  let userCredentials = req.body;
+  const authUser = login(userCredentials);
+  if (authUser) {
+    res.send({ token: authUser });
+  } else {
+    res.send({ error: "invalid credential" });
+  }
+});
+app.post("/admin/isAuthenticated", async (req, res) => {
+  console.log("called");
+  const userToken = req.body.token;
+  res.send({ isAuthenticated: isAuthenticated(userToken) });
+});
 
 app.post("/order", (req, res) => {
   let order = req.body;
