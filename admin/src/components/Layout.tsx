0b6fced36ae800d3d29cd613ecbd9b2f7d0f1106
gustavosf2003 <gsferreira2003@gmail.com>
2023-05-06 15:33:59 +0100

    fix: layout component

--- admin/src/components/Layout.tsx
+++ admin/src/components/Layout.tsx
@@ -1,12 +1,18 @@
 import { SideMenu } from "@/components/SideMenu";
 import useAuth from "@/hooks/useAuth";
+import clsx from "clsx";
 
-export default function Layout({ children }) {
+export default function Layout({ children, className }) {
   useAuth();
   return (
     <div className="relative flex">
       <SideMenu />
-      <div className="bg-gray-100 min-h-screen w-full p-8 flex flex-col gap-8 ml-[248px]">
+      <div
+        className={clsx(
+          "bg-gray-100 min-h-screen w-full p-8 flex flex-col gap-8 ml-[248px]",
+          className
+        )}
+      >
         {children}
       </div>
     </div>
