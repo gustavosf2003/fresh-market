cb7e359c415668d93e335b2866b2152699ea6865
gustavosf2003 <gsferreira2003@gmail.com>
2023-05-06 02:32:44 +0100

    feat: add pages

---
+++ admin/src/pages/orders.tsx
@@ -0,0 +1,6 @@
+import Layout from "@/components/Layout";
+import useAuth from "@/hooks/useAuth";
+
+export default function Orders() {
+  return <Layout>Orders</Layout>;
+}
---
+++ admin/src/pages/users.tsx
@@ -0,0 +1,6 @@
+import Layout from "@/components/Layout";
+import useAuth from "@/hooks/useAuth";
+
+export default function Users() {
+  return <Layout>Users</Layout>;
+}
